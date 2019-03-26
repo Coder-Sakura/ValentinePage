e1714f2a064203402bfd3cc7e30db642bf13cdd3
Coder_桜 <1508015265@qq.com>
2019-03-26 19:18:39 +0800

    Site updated: 2019-03-26 19:18:38

--- js/src/custom.js
+++
@@ -1,7 +0,0 @@
-/* 轮播背景图片 */
-$(function () {
-    $.backstretch([  
-          "/images/background/background.jpg",
-          "/images/background/background1.jpg"
-    ], { duration: 10000, fade: 1500 });  
-});
