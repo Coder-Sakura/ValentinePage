d68924fd3a0236df86986f567fe717e6163cb8df
Coder_Sakura <1508015265@qq.com>
2019-09-17 20:00:55 +0800

    Site updated: 2019-09-17 20:00:47

---
+++ js/src/custom.js
@@ -0,0 +1,7 @@
+/* 轮播背景图片 */
+$(function () {
+    $.backstretch([  
+          "/images/background/background.jpg",
+          "/images/background/background1.jpg"
+    ], { duration: 10000, fade: 1500 });  
+});
